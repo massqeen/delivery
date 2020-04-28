3b682d85bb1ff03b3f764f3b1b74e8b44b3f7089
massqeen <62159695+massqeen@users.noreply.github.com>
2020-04-28 20:37:36 +0300

    fixed bug with main background, added 'close modal' action to cancel button

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 const cartButton = document.querySelector(
     '#cart-button'
 )
+const cancel = document.querySelector(
+    '#cart-cancel'
+)
 const modal = document.querySelector('.modal')
 const close = document.querySelector('.close')
 
 cartButton.addEventListener('click', toggleModal)
 
 close.addEventListener('click', toggleModal)
+
+cancel.addEventListener('click', toggleModal)
 
 function toggleModal() {
     modal.classList.toggle('is-open')
